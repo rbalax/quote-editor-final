71721dc4da68de67d42587056a5059ca4e5062f4
Rom BALAX <rbalax@yahoo.co.uk>
2024-05-13 15:41:16 +0900

    devise and signed_streams done

--- Gemfile
+++ Gemfile
@@ -49,6 +49,9 @@ gem "bootsnap", require: false
 
 gem "simple_form", "~> 5.1.0"
 
+# ADDED GEMS
+gem "devise", "~> 4.8.1"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,6 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
+  belongs_to :company
 
   scope :ordered, -> { order(id: :desc) }
 
@@ -7,6 +8,6 @@ class Quote < ApplicationRecord
   # after_create_commit -> { broadcast_prepend_to "quotes" }
   # after_update_commit -> { broadcast_replace_to "quotes" }
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
-broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 # broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
